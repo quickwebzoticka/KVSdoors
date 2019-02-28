dc02d9785fb18a65bf25abc16b86616d08a5e2d9
Andrey Sokolow <quick.sokolow94@gmail.com>
2019-02-28 18:46:43 +0300

    v1.0 track added

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,50 +1,31 @@
 $(document).ready(function(){
+
+	function closeAllDoors() {
+		$('.door').removeClass('opened');
+		$('.door-item').attr('style', '');
+		$('.door-description').removeClass('active');
+		$('.doors-inn-item').removeClass('visible');
+	};
+
 	$(document).on('click', '.doors-item-wrapper', function() {
 		let temp = $(this).find('.door').hasClass('opened');
 
-		$('.door').removeClass('opened');
-		$('.door-item_1, .door-item_2, .door-item_3').attr('style', '');
-		$('.door-description').removeClass('active');
-		$('.doors-inn-item').removeClass('visible');
+		closeAllDoors();
 
 		if (temp) {
-
-
 			$(this).find('.door').removeClass('opened');
 			$(this).find('.door-description').removeClass('active');
 			$(this).find('.doors-inn-item').removeClass('visible');
 
-
-			if ( $(this).index('.doors-item-wrapper') == 0 ) {
-				$('.door-item_1').attr('style', '');
-				return false;
-			}
-			if ( $(this).index('.doors-item-wrapper') == 1 ) {			
-				$('.door-item_2').attr('style', '');
-				return false;
-			}
-			if ( $(this).index('.doors-item-wrapper') == 2 ) {
-				$('.door-item_3').attr('style', '');
-				return false;
-			}
+			$(this).find('.door-item').attr('style', '');
 			return false;
 		}
 
 		$(this).find('.door').addClass('opened');
 		$(this).find('.door-description').addClass('active');
 		$(this).find('.doors-inn-item').addClass('visible');
-		if ( $(this).index('.doors-item-wrapper') == 0 ) {
-			$('.door-item_1').css({left: '0px'});
-			return false;
-		}
-		if ( $(this).index('.doors-item-wrapper') == 1 ) {			
-			$('.door-item_2').css({left: `370px`});
-			return false;
-		}
-		if ( $(this).index('.doors-item-wrapper') == 2 ) {
-			$('.door-item_3').css({left: '800px'});
-			return false;
-		}
+
+		$(this).find('.door-item').css({left: '0px', transform: 'translateX(-70%)'});
 	});
 
 	$(document).on('click', '.door-description', function() {
@@ -54,5 +35,25 @@ $(document).ready(function(){
 	$(document).on('click', '.door-description__close', function(e) {
 		e.preventDefault();
 		$(this).closest('.door-description').removeClass('active');
+	});
+
+	$(document).on('click', '.doors-wrapper-arrow_next', function() {
+		let temp = $('.doors-wrapper-track').css('transform');
+
+		temp = temp.split(',');
+		temp = temp[temp.length-2];
+		temp = temp - 325;
+		$('.doors-wrapper-track').css({transform: `translateX(${temp}px)`});
 	})
+
+	$(document).on('click', '.doors-wrapper-arrow_prev', function() {
+		let temp = $('.doors-wrapper-track').css('transform');
+
+		temp = temp.split(',');
+		temp = temp[temp.length-2];
+		temp = parseInt(temp) + 325;
+		$('.doors-wrapper-track').css({transform: `translateX(${temp}px)`});
+	})
+
+	
 });
