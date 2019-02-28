d82d74a758d7d02e2ff008f18eded2798a53475c
Andrey Sokolow <quick.sokolow94@gmail.com>
2019-02-28 12:30:02 +0300

    v1.0

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,21 +1,58 @@
 $(document).ready(function(){
-	$(document).on('click', '.door', function() {
-		$(this).css({transform: 'rotate3d(0, 1, 0, 75deg)'});
+	$(document).on('click', '.doors-item-wrapper', function() {
+		let temp = $(this).find('.door').hasClass('opened');
 
-		if ( $(this).index('.door') == 0 ) {
+		$('.door').removeClass('opened');
+		$('.door-item_1, .door-item_2, .door-item_3').attr('style', '');
+		$('.door-description').removeClass('active');
+		$('.doors-inn-item').removeClass('visible');
+
+		if (temp) {
+
+
+			$(this).find('.door').removeClass('opened');
+			$(this).find('.door-description').removeClass('active');
+			$(this).find('.doors-inn-item').removeClass('visible');
+
+
+			if ( $(this).index('.doors-item-wrapper') == 0 ) {
+				$('.door-item_1').attr('style', '');
+				return false;
+			}
+			if ( $(this).index('.doors-item-wrapper') == 1 ) {			
+				$('.door-item_2').attr('style', '');
+				return false;
+			}
+			if ( $(this).index('.doors-item-wrapper') == 2 ) {
+				$('.door-item_3').attr('style', '');
+				return false;
+			}
+			return false;
+		}
+
+		$(this).find('.door').addClass('opened');
+		$(this).find('.door-description').addClass('active');
+		$(this).find('.doors-inn-item').addClass('visible');
+		if ( $(this).index('.doors-item-wrapper') == 0 ) {
 			$('.door-item_1').css({left: '0px'});
 			return false;
 		}
-		if ( $(this).index('.door') == 1 ) {
-			let temp = $('.doors-item').eq(1).offset().left;
-			
-			$('.door-item_2').css({left: `${(temp - 155)}px`});
+		if ( $(this).index('.doors-item-wrapper') == 1 ) {			
+			$('.door-item_2').css({left: `370px`});
 			return false;
 		}
-		if ( $(this).index('.door') == 2 ) {
-			$('.door-item_1').css({left: '0px'});
+		if ( $(this).index('.doors-item-wrapper') == 2 ) {
+			$('.door-item_3').css({left: '800px'});
 			return false;
 		}
-		
+	});
+
+	$(document).on('click', '.door-description', function() {
+		return false;
+	});
+
+	$(document).on('click', '.door-description__close', function(e) {
+		e.preventDefault();
+		$(this).closest('.door-description').removeClass('active');
 	})
 });
