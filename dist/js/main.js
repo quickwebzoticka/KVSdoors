a3a46711980843ccff56b695c2e0981a98b0216c
Andrey Sokolow <quick.sokolow94@gmail.com>
2019-03-01 13:57:21 +0300

    finished v1.1

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
 		$('.door').removeClass('opened');
 		$('.door-item').attr('style', '');
 		$('.door-description').removeClass('active');
+		$('.door-description').removeClass('door-description_left');
 		$('.doors-inn-item').removeClass('visible');
 	};
 
@@ -23,6 +24,9 @@ $(document).ready(function(){
 
 		$(this).find('.door').addClass('opened');
 		$(this).find('.door-description').addClass('active');
+		if ($('.door-description.active').offset().left > 1000) {
+			$('.door-description.active').addClass('door-description_left');
+		}
 		$(this).find('.doors-inn-item').addClass('visible');
 
 		$(this).find('.door-item').css({left: '0px', transform: 'translateX(-70%)'});
@@ -37,21 +41,57 @@ $(document).ready(function(){
 		$(this).closest('.door-description').removeClass('active');
 	});
 
+	let trackWidth = document.querySelectorAll('.doors-wrapper-track')[0].offsetWidth;
+	let wrapperWidth = $('.doors-wrapper').width();
+
 	$(document).on('click', '.doors-wrapper-arrow_next', function() {
+		$(this).css('pointer-events', 'none');
+		setTimeout(() => {
+			$(this).css('pointer-events', 'auto');
+		}, 300);
+
+		$('.doors-wrapper-arrow_prev').removeClass('disabled');
+
 		let temp = $('.doors-wrapper-track').css('transform');
+		let tempResult = ((trackWidth)*(-1) + wrapperWidth - 100);
 
 		temp = temp.split(',');
 		temp = temp[temp.length-2];
-		temp = temp - 325;
+		temp = parseInt(temp) - 330;
+
+
+		if (temp <= tempResult) {
+			$('.doors-wrapper-arrow_next').addClass('disabled');
+			return false;
+		} else {
+			$('.doors-wrapper-arrow_next').removeClass('disabled');
+		}
+
 		$('.doors-wrapper-track').css({transform: `translateX(${temp}px)`});
 	})
 
 	$(document).on('click', '.doors-wrapper-arrow_prev', function() {
+		$(this).css('pointer-events', 'none');
+		setTimeout(() => {
+			$(this).css('pointer-events', 'auto');
+		}, 300);
+
 		let temp = $('.doors-wrapper-track').css('transform');
 
+
+		$('.doors-wrapper-arrow_next').removeClass('disabled');
 		temp = temp.split(',');
 		temp = temp[temp.length-2];
-		temp = parseInt(temp) + 325;
+		temp = parseInt(temp) + 330;
+
+		if (temp > 0) {
+			$('.doors-wrapper-arrow_prev').addClass('disabled');
+
+			return false;
+		} else {
+			$('.doors-wrapper-arrow_prev').removeClass('disabled');
+		}
+
 		$('.doors-wrapper-track').css({transform: `translateX(${temp}px)`});
 	})
 
